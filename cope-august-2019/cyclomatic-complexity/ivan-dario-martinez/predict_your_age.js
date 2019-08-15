00d7b81877cf50c704bc47ffe4a71db237d915dd
ivan <imartinezgrandos@gmail.com>
2019-08-15 12:28:47 -0500

    Predict your age
    Js script

---
+++ cope-august-2019/cyclomatic-complexity/ivan-dario-martinez/predict_your_age.js
@@ -0,0 +1,9 @@
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+    var total = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i] * arguments[i];
+    }
+
+    return parseInt(Math.sqrt(total) / 2);
+}
