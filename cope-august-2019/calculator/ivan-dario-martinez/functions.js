7291b8c48783be1a5366768523637bc2e93344a2
ivan <imartinezgrandos@gmail.com>
2019-08-16 15:57:50 -0500

    Calculator created over JavaScript and HTML

---
+++ cope-august-2019/calculator/ivan-dario-martinez/functions.js
@@ -0,0 +1,34 @@
+var operationType = "";
+
+function operate() {
+    var res = 0;
+    var a = Number(document.getElementById("valueA").value);
+    var b = Number(document.getElementById("valueB").value);
+    if (a != "" && b != "") {
+        switch (this.operationType) {
+            case "+":
+                res = a + b
+                break;
+            case "-":
+                res = a - b
+                break;
+            case "*":
+                res = a * b
+                break;
+            case "/":
+                res = a / b
+                break;
+
+            default:
+                alert("Please select a type of operation");
+                break;
+        }
+        document.getElementById("result").value = res;
+    }
+}
+
+function clickOperationButton(param) {
+    this.operationType = param;
+    document.getElementById("operation").value = this.operationType;
+    operate();
+}
