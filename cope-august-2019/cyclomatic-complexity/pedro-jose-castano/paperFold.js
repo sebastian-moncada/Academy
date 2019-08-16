57ca2a2de16d76cbb53a1d810a211390c9720207
pedrocatano <54031297+pedrocatano@users.noreply.github.com>
2019-08-16 10:46:31 -0500

    Fix a bug related with the paperFold algorithm

--- cope-august-2019/cyclomatic-complexity/pedro-jose-castano/paperFold.js
+++ cope-august-2019/cyclomatic-complexity/pedro-jose-castano/paperFold.js
@@ -11,7 +11,7 @@ function* paperFold() {
     // Add the next sequence to the array
     for(let i=0; i<10000; i++) {
         // Add the previous sequence, plus a new 1, plus the reversed previous sequence
-        sequences.push(sequences[i] + "1" + sequences[i].split("").map(x => 1-x).join(""));
+        sequences.push(sequences[i] + "1" + sequences[i].split("").reverse().map(x => 1-x).join(""));
         yield sequences[i];
     }
 }
