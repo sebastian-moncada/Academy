80b15021bcb1a190731a949e82608d4455a2a33e
andrestamayoglobant <54039392+andrestamayoglobant@users.noreply.github.com>
2019-08-16 10:19:55 -0500

    upgrades
    Se mejoró la funcion paperFold, de tal manera que pasara las pruebas unitarias ( la anterior version imprimia una cadena completa  y no digito por digito) 
    Nota: no se hicieron adjuntaron los graficos de complejidad, en lugar de ello en cada funcion se adjunto la formula con los valores y su respectivo calculo

--- cope-august-2019/cyclomatic-complexity/andres-felipe-tamayo/dragonCurve.js
+++ cope-august-2019/cyclomatic-complexity/andres-felipe-tamayo/dragonCurve.js
@@ -5,24 +5,52 @@
     // M = 2
 function* paperFold() {
         
-    let sec ="1";
-    const run = true;
-    yield sec;
+    let workWith= generateSecuence([1],1);
+    let tempArr =workWith.array;
+    let lastLetter =workWith.lastLetter;
 
-    while( run){
-        let arraySec="";
-        lastLetter="1";
-        for(i=0;i<sec.length;i++){
-            arraySec=arraySec+lastLetter;
-            arraySec=arraySec+sec[i];
-
-            if(lastLetter==="1")
-                lastLetter="0";
-            else
-                lastLetter="1";
+    for (let i = 0; i < tempArr.length; i++) {
+        if(i==tempArr.length-1){
+            let sec = tempArr;
+            yield tempArr[i];
+            workWith=generateSecuence(sec,lastLetter);
+            tempArr =workWith.array;
+            lastLetter =workWith.lastLetter;
         }
-        sec= arraySec;
-        yield sec;
+        else
+            yield tempArr[i];
     }
+}
+
+
+    // cyclomatic complexity 
+    // M = E-N +1
+    // M = 11-9 + 1
+    // M = 3
+
+/**
+ * Generates dragon secuence
+ * @param {*} sec  actual secuence
+ * @param {*} control last digit control
+ */
+function generateSecuence(sec,control){
+
+    var tempArr=[];
+    let retorno={};
+    for (var i = 0; i < sec.length; i++) {
+
+        tempArr.push(control);
+        tempArr.push(sec[i]);
+
+       (control==1) ? control=0 : control=1;
+    }
+        tempArr.push(control);
+        (control==1) ? control=0 : control=1;
+    
+    retorno ={
+        "array": tempArr,
+        "lastLetter":control
+    }
+    return retorno;
 }
 
