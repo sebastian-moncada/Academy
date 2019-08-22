8c842993703d5e99a479ff77b7227315365a4d4f
Ivan Bustos <contacto@ivanbustos.com>
2019-08-22 16:30:01 -0500

    Added interactivity to Canvas puzzle.

--- cope-august-2019/sliding-puzzle/ivan-bustos/main.js
+++ cope-august-2019/sliding-puzzle/ivan-bustos/main.js
@@ -1,7 +1,10 @@
 var settings = {
   box_border: 2,
   box_border_color: "#7fAC8C",
+  box_border_error_color: "#ff425f",
+  box_border_success_color: "#b8e3c4",
   box_color: "#679e76",
+  box_hover_color: "#78AF87",
   box_void_color: "#313b33",
   box_padding: 10,
   box_width: 100,
@@ -10,40 +13,136 @@ var settings = {
   label_color: "#9edbaf",
   label_font: "12px Arial",
   game_size: 3,
-  randomized: true
+  randomized: true,
 };
 
+var map_labels = [];
+
 /**
- * Yields a box setting so it can be drawn.
+ * Determines if void is adjacent to void box.
  */
-function *calculateBoxSettings($randomized = false) {
+let isAdjacentToVoid = function($pos) {
+  let $void_delta, $voidx, $voidy, $x, $y;
+
+  let findX = function($i) {
+    let $x = parseInt(($i + 1) % settings.game_size);
+    if ($x === 0) {
+      $x = settings.game_size;
+    }
+    return $x;
+  }
+
+  let findY = function($i) {
+    return (Math.ceil(($i + 1) / settings.game_size ) * settings.game_size) / settings.game_size;
+  }
+
+  // Let's find the void coordinates.
+  $void_delta = map_labels.indexOf('v');
+  $voidx = findX($void_delta);
+  $voidy = findY($void_delta);
+
+  // Let's determine if $pos is adjacent to void.
+  $x = findX($pos);
+  $y = findY($pos);
+
+  if ($x == $voidx) {
+    if (($y + 1 == $voidy) || ($y - 1 == $voidy)) {
+      return true;
+    }
+  }
+  if ($y == $voidy) {
+    if (($x + 1 == $voidx) || ($x - 1 == $voidx)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Determines if point is within box boundaries.
+ */
+let isPointWithinABox = function($x, $y, $box_x, $box_y) {
+  if ($x >= $box_x && $x <= ($box_x + settings.box_width)) {
+    if ($y >= $box_y && $y <= ($box_y + settings.box_width)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Yields a single box data so it can be drawn.
+ */
+function *calculateBoxSettings($event = null) {
   let $box_qty = Math.pow(settings.game_size, 2);
   let $x = settings.box_border;
   let $y = settings.box_border;
-  let $labels = [];
+  let $redraw = false;
 
-  // Create the labels.
-  for ($i = 1; $i < $box_qty; $i++) {
-    $labels.push($i);
+  // Create the labels if not yet created.
+  if (map_labels.length != $box_qty) {
+    for (let $i = 1; $i < $box_qty; $i++) {
+      map_labels.push($i);
+    }
+
+    // Add the void.
+    map_labels.push("v");
+
+    // Randomized if requested.
+    if (settings.randomized) {
+      map_labels.sort(() => Math.random() - 0.5);
+    }
+    map_labels = map_labels;
   }
 
-  // Add the void.
-  $labels.push("v");
-
-  // Randomized if requested.
-  if ($randomized) {
-    $labels.sort(() => Math.random() - 0.5);
-  }
-
-  $printed_count = 0;
+  let $printed_count = 0;
 
   // Yield a box setting.
-  for ($i = 0; $i < $box_qty; $i++) {
-    $config = {
-      "l": $labels[$i],
+  for (let $i = 0; $i < $box_qty; $i++) {
+    let $box_color = settings.box_color;
+    let $box_border = settings.box_border_color;
+
+    if ($event) {
+      let $is_over_box = isPointWithinABox($event.layerX, $event.layerY, $x, $y);
+      switch ($event.type) {
+        case 'mousemove':
+          if ($is_over_box) {
+            $box_color = settings.box_hover_color;
+          }
+          break;
+
+        case 'mousedown':       
+          if ($is_over_box) {
+            $box_color = settings.box_hover_color;
+            $box_border = settings.box_border_error_color;
+            if (isAdjacentToVoid($i)) {
+              $box_border = settings.box_border_success_color;
+            }
+          }
+          break;
+
+        case 'mouseup':
+          if ($is_over_box && isAdjacentToVoid($i)) {
+            let $void_delta = map_labels.indexOf('v');
+            map_labels[$void_delta] = map_labels[$i];
+            map_labels[$i] = 'v';
+            if ($void_delta < $i) {
+              $i = -1;
+              $redraw = true;
+            }
+          }
+          break;
+      }
+    }
+      
+    let $config = {
+      "l": map_labels[$i],
       "x": $x,
       "y": $y,
-      "is_void": $labels[$i] === "v"
+      "is_void" : map_labels[$i] === 'v',
+      "c" : $box_color,
+      "b" : $box_border,
+      "r": $redraw
     }
     yield $config;
 
@@ -70,29 +169,32 @@ function *calculateBoxSettings($randomized = false) {
 }
 
 /**
- * Draws boxes.
+ * Draws all boxes.
  */
-function drawBoxes() {
+let draw = function ($event = null) {
   let $canvas = document.getElementById(settings.canvas_id);
   let $ctx = $canvas.getContext("2d");
-  let $generator = calculateBoxSettings(settings.randomized);
+  let $generator = calculateBoxSettings($event);
   while ($config = $generator.next().value) {
+    // A box has requested to redraw.
+    if ($config.r) {
+      draw($event);
+      break;
+    }
     // Create the box.
     $ctx.beginPath();
     $ctx.lineWidth = settings.box_border;
-    $ctx.strokeStyle = $config.is_void ? settings.box_void_color : settings.box_border_color;
-    $ctx.fillStyle = $config.is_void ? settings.box_void_color : settings.box_color;
+    $ctx.strokeStyle = $config.is_void ? settings.box_void_color : $config.b;
+    $ctx.fillStyle = $config.is_void ? settings.box_void_color : $config.c;
     $ctx.rect($config.x, $config.y, settings.box_width, settings.box_width);
     $ctx.fill();
     $ctx.stroke();
 
     if (!$config.is_void) {
       // Add the text.
-      $ctx.font = "12px Arial"; 
+      $ctx.font = settings.label_font; 
       $ctx.textAlign = "center";
       $ctx.fillStyle = settings.label_color;
-      //console.log(parseInt($config.x + (settings.box_width / 2)));
-      //console.log(parseInt($config.y + (settings.box_width / 2)));
       $ctx.fillText($config.l, parseInt($config.x + (settings.box_width / 2)), parseInt($config.y + (settings.box_width / 2)));
     }
   }
@@ -101,7 +203,7 @@ function drawBoxes() {
 /**
  * Initializes canvas.
  */
-function initCanvas() {
+let initCanvas = function () {
   let $canvas = document.getElementById(settings.canvas_id);
   let $base = settings.box_width * settings.game_size;
   let $border = settings.game_size * 2 * settings.box_border;
@@ -124,5 +226,18 @@ function initCanvas() {
  */
 document.addEventListener("DOMContentLoaded", function() {
   initCanvas();
-  drawBoxes();
+  draw();
+  let $canvas = document.getElementById(settings.canvas_id);
+  $canvas.addEventListener('mousemove', draw);
+  $canvas.addEventListener('mouseout', draw);
+  $canvas.addEventListener('mousedown', draw);
+  $canvas.addEventListener('mouseup', draw);
+
+  document.getElementById('restart').addEventListener('submit', function(event) {
+    event.preventDefault();
+    settings.game_size = parseInt(document.getElementById('mapsize').value);
+    map_labels = [];
+    initCanvas();
+    draw();
+  });
 });
