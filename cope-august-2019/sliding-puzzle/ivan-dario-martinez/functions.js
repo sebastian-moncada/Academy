6f6c45505f1062bb8d8441dd8e4681ab6345f820
ivan <imartinezgrandos@gmail.com>
2019-08-22 17:22:27 -0500

    Improvements to slide puzzle, functions to mix, restart and verify if it's rightly organized

--- cope-august-2019/sliding-puzzle/ivan-dario-martinez/functions.js
+++ cope-august-2019/sliding-puzzle/ivan-dario-martinez/functions.js
@@ -17,6 +17,7 @@ function cellClick(x, y) {
             }
         }
     }
+    validateCells();
 }
 
 let validate = (cell) => {
@@ -28,8 +29,49 @@ let validate = (cell) => {
     }
 }
 
+/**
+ * @param {HTMLObjectElement} containerEmpty  Empty cell
+ * @param {HTMLObjectElement} contanierValue  Cell to swap for the empty cell
+ */
+
 function swapCells(containerEmpty, contanierValue) {
     var temp = contanierValue.getElementsByClassName("cell")[0];
     contanierValue.appendChild(document.getElementById('cellxx'));
     containerEmpty.appendChild(temp);
+}
+
+function mix() {
+    for (let index = 0; index < 64; index++) {
+        var containerEmpty = document.getElementById('cellxx').parentNode;
+        var x = Math.floor((Math.random() * 4) + 1);
+        var y = Math.floor((Math.random() * 4) + 1);
+        var contanierValue = document.getElementById(`cont${x}${y}`);
+        swapCells(containerEmpty, contanierValue);
+    }
+    validateCells();
+}
+
+function restart() {
+    var container = document.getElementById(`cont44`);
+    container.appendChild(document.getElementById(`cellxx`));
+    for (let x = 1; x < 5; x++) {
+        for (let y = 1; y < 5; y++) {
+            if (x !== 4 || y !== 4) {
+                var container = document.getElementById(`cont${x}${y}`);
+                container.appendChild(document.getElementById(`cell${x}${y}`));
+            }
+        }
+    }
+    validateCells();
+}
+
+function validateCells() {
+    var containers = document.getElementsByClassName("columnContainer");
+    for (const element of containers) {
+        if (element.getElementsByClassName("cell")[0].id.substring(4, 6) == element.id.substring(4, 6)) {
+            element.getElementsByClassName("cell")[0].classList.add("cellOk");
+        } else {
+            element.getElementsByClassName("cell")[0].classList.remove("cellOk");
+        }
+    }
 }
